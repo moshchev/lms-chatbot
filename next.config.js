6332944ed575cda2d9a35cf9d996be3c70a4ff7f
Alexander Moshchev <118296062+moshchev@users.noreply.github.com>
2025-03-12 15:25:35 +0100

    Update Node.js version to 18.18.0 in configuration files and remove deprecated next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Configure for Cloudflare Pages with Functions
-  experimental: {
-    appDir: true,
-  },
-}
-
-module.exports = nextConfig 
